98e6e1876514798b382f18207fd540bc6ef4094b
kanishak saini <kanishaksaini63@gmail.com>
2026-06-22 20:16:44 +0530

    intersection of LL

--- Leetcodeques.cpp
+++ Leetcodeques.cpp
@@ -20,3 +20,22 @@ sort(s.begin(),s.end());
         return head;
     }
 };*/
+
+//160
+/*class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* ptr1 = headA;
+        ListNode* ptr2 = headB;
+        unordered_set<ListNode*> nodes_in_A;
+        unordered_set<ListNode*> nodes_in_B;
+        while(ptr1!=nullptr){
+            nodes_in_A.insert(ptr1);
+            ptr1 = ptr1->next;
+        }
+        while(ptr2!=nullptr){
+            if(nodes_in_A.count(ptr2)) return ptr2;
+            ptr2=ptr2->next;
+        }return nullptr;
+    }
+};*/
