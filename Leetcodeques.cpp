8b1f08cf97cdfdd4a6aa0f6a7951b3ae0a832125
kanishak saini <kanishaksaini63@gmail.com>
2026-06-27 12:32:41 +0530

    ques22

--- Leetcodeques.cpp
+++ Leetcodeques.cpp
@@ -188,4 +188,30 @@ public:
         return dummy.next;
     }
 };
+                                           //22
+class Solution {
+    vector <string> ans ;
+public:
+    void solve(int open , int close , int n , string curr){
+        if(curr.length()==2*n){
+        ans.push_back(curr);
+        curr = "";
+        }
+
+        if(open<n){
+            solve(open+1, close , n , curr + '(');
+        }
+
+        if(close<open){
+            solve(open,close+1,n,curr+')');
+        }
+    }
+
+
+    vector<string> generateParenthesis(int n) {
+        string curr ="";
+        solve(0,0,n,curr);
+        return ans;
+    }
+};
 */
