221eabe616d498d4fec3744527b0ede2a4a59fb3
kanishak saini <kanishaksaini63@gmail.com>
2026-07-04 15:45:25 +0530

    51 ques

--- Leetcodeques.cpp
+++ Leetcodeques.cpp
@@ -409,4 +409,47 @@ public:
         }return false;
     }
 };
+                                 //51 ques
+class Solution {
+public:
+
+    void solve(int col , vector <vector<string>> &ans, vector<string> &board , int n ,vector <int> &leftrow , vector<int> &UpperDiagonal , vector <int> &LowerDiagonal){
+
+
+        if(col==n){
+            ans.push_back(board);
+            return ;
+        }
+
+        for( int row= 0 ; row <n ; row ++){
+            if(leftrow[row]==0 && UpperDiagonal[(n-1)+(col-row)]==0 && LowerDiagonal[row+col]==0) {
+                board[row][col] = 'Q';
+                leftrow[row] = 1;
+                UpperDiagonal[(n-1)+(col-row)] = 1;
+                LowerDiagonal[row + col] = 1;
+                solve(col +1 , ans,board ,n ,leftrow , UpperDiagonal , LowerDiagonal);
+
+                board[row][col] = '.';
+                leftrow[row] = 0;
+                UpperDiagonal[(n-1)+(col-row)] = 0;
+                LowerDiagonal[row + col] = 0;
+            }
+        }
+
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector <vector<string>> ans;
+        vector <string> board(n) ;
+        string s(n,'.');
+        for ( int i =0 ; i< n ;i++){
+            
+               board[i] = s;
+            
+        }
+        vector<int> leftrow(n,0) , UpperDiagonal(n*2-1,0) , LowerDiagonal(2*n-1,0);
+        solve(0,ans,board,n,leftrow,UpperDiagonal,LowerDiagonal);
+
+        return ans;
+    }
+};
 */
