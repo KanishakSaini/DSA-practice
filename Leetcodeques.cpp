a6529a313ba05278f59dbda5efa2d0597d7c9fb9
kanishak saini <kanishaksaini63@gmail.com>
2026-07-10 11:47:32 +0530

    2220 que

--- Leetcodeques.cpp
+++ Leetcodeques.cpp
@@ -516,5 +516,18 @@ long long n = llabs((long long)dividend);
 
         return sign? ans: -ans;
     }
+};                                   
+                                 //2220
+                                 class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+      int x = start^goal;
+      int count = 0;
+      while(x>0){
+        count += (x & 1);
+        x >>= 1;
+      }
+        return count;
+    }
 };
 */
