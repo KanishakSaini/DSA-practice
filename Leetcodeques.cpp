dd7987f61c7684992b74f9feb447ca87c050bf7d
kanishak saini <kanishaksaini63@gmail.com>
2026-07-13 12:31:16 +0530

    204

--- Leetcodeques.cpp
+++ Leetcodeques.cpp
@@ -539,5 +539,44 @@ public:
             result ^=num;
         }return  result;
     }
+};/*
+                             //204
+                             class Solution {
+public:
+    int countPrimes(int n) {
+      /*  if (n < 2)
+            return 0;
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            bool flag = true;
+            for (int j = 2; j * j <= i; j++) {
+                if (i % j == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag == true)
+                count++;
+        }
+        return count;
+
+        if(n<=2) return 0;
+        vector<bool> isprime(n,true);
+        isprime[0] = false; //0,1 are not applicable in this  ques
+        isprime[1] = false;
+        
+        for(int i = 2 ; i*i< n ; i++){ // marking false to square of primes
+            if(isprime[i]){
+                for(int j = i*i ; j < n ; j += i){
+                    isprime[j] = false;
+                }
+            }
+        }
+        
+        int count = 0 ;
+        for(int i = 2 ; i<n ; i++){
+            if(isprime[i]) count++;
+        } 
+        return  count ;  }
 };
 */
