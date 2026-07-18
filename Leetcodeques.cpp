7fb7680d6e6909910efca4a20db265067304494e
kanishak saini <kanishaksaini63@gmail.com>
2026-07-18 13:49:38 +0530

    503 ques

--- Leetcodeques.cpp
+++ Leetcodeques.cpp
@@ -669,5 +669,27 @@ public:
         }
         return result;
         }
-    };
+    }; 
+                                               //503
+                                               class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        stack <int> st;
+        int n = nums.size();
+        vector <int> ans(n,-1);
+        for(int i= 2*n-1 ; i>=0 ; i--){
+            int idx = i%n;
+            while(!st.empty() && nums[st.top()] <= nums[idx]){
+                st.pop();
+            }
+
+            if(!st.empty()) {
+                ans[idx] = nums[st.top()];
+            }
+
+            st.push(idx);
+        }
+       return ans;
+    }
+};
 */
