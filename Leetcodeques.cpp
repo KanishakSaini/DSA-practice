98b13affdad1c31407a0b6ec1cec24d5b72610cd
kanishak saini <kanishaksaini63@gmail.com>
2026-07-23 22:55:10 +0530

    1

--- Leetcodeques.cpp
+++ Leetcodeques.cpp
@@ -766,5 +766,20 @@ public:
             sum = (sum +(1LL*arr[i]*leftchoice)%MOD*rightchoice)%MOD ;
         }return sum ;
     }
+}; 
+                                                    //1
+                                                    class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        for (int i = 0; i < n-1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
 };
 */
