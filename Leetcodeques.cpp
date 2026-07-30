b612d31b8e31b182d4c909ea805d8073d5678ea4
kanishak saini <kanishaksaini63@gmail.com>
2026-07-30 21:39:26 +0530

    189

--- Leetcodeques.cpp
+++ Leetcodeques.cpp
@@ -801,4 +801,19 @@ while(x!=0){
 }return false;
 }
 };
+ //189
+ class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        if (n == 0) return;
+
+        k = k % n;
+
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
+    }
+};
+
 */
